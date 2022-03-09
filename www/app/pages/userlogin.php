3c22fbc4d0a93efaf99c493f1a5d387775b264f4
leon.mbs@gmail.com <leon.mbs@gmail.com>
2022-03-09 13:04:30 +0200

    fix

--- www/app/pages/userlogin.php
+++ www/app/pages/userlogin.php
@@ -37,17 +37,7 @@ class UserLogin extends \Zippy\Html\WebPage
         //проверка  новой версии        
         $this->_tvars['isnewversion'] = false;
 
-        $v = @file_get_contents("https://zippy.com.ua/version.json?t=" . time());
-        $v = @json_decode($v, true);
-        if (strlen($v['version']) > 0) {
-            $c = (int)str_replace(".", "", str_replace("v", "", $version));
-            $n = (int)str_replace(".", "", str_replace("v", "", $v['version']));
-            if ($n > $c) {
-                $this->_tvars['isnewversion'] = true;
-            }
-
-            $this->_tvars['newversion'] = $v['version'];
-        }
+ 
 
         $this->_tvars['appname'] = $common['shopname'];
         $this->_tvars['capcha'] = $common['capcha'] == 1;
